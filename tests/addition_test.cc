0ee1c212bbc4d3dcd92afba0d016e1f381478db4
Grégory DAVID <groolot@groolot.net>
2020-03-11 16:33:11 +0100

    Modularisation des tests unitaires
    Chaque classe de test dispose de son propre programme principal et évite ainsi d'avoir tous les tests unitaires rassemblés dans le seul programme 'testarithmetique'. Dés lors, le résumé des tests devient plus clair car il rapporte la réussite/échec de chacun des tests en fin d'exécution des tests.

--- tests/addition_test.cc
+++ tests/addition_test.cc
@@ -4,6 +4,10 @@
   #include "config.h"
 #endif
 
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
 #include <limits>
 
 // Enregistrer la classe de test dans le registre de la suite
@@ -55,4 +59,22 @@ void additionTest::addition_zero()
   CPPUNIT_ASSERT_EQUAL(static_cast<long int>(0),
                        static_cast<long int>(arithmetique::addition(operandeA, operandeB))
     );
+}
+
+int main()
+{
+  // Obtenir le registre de tests
+  CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+  // Obtenir la suite principale depuis le registre
+  CppUnit::Test *suite = registry.makeTest();
+  // Ajouter la suite à l'exécutant des tests
+  CppUnit::TextUi::TestRunner runner;
+  runner.addTest(suite);
+  // Définir l'utilitaire de sortie au format "erreurs de compilation"
+  runner.setOutputter(new CppUnit::CompilerOutputter(&runner.result(),
+                      std::cerr));
+  // Lancer l'exécution des tests
+  bool wasSucessful = runner.run();
+  // Retourner le code d'erreur 1 si l'un des tests a échoué
+  return wasSucessful ? 0 : 1;
 }
--- tests/division_test.cc
+++ tests/division_test.cc
@@ -4,6 +4,10 @@
   #include "config.h"
 #endif
 
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
 #include <limits>
 
 // Enregistrer la classe de test dans le registre de la suite
@@ -16,3 +20,21 @@ void divisionTest::tearDown() {}
 /* TODO: Ajouter les définitions des méthodes de test de la classe
  * divisionTest
  */
+
+int main()
+{
+  // Obtenir le registre de tests
+  CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+  // Obtenir la suite principale depuis le registre
+  CppUnit::Test *suite = registry.makeTest();
+  // Ajouter la suite à l'exécutant des tests
+  CppUnit::TextUi::TestRunner runner;
+  runner.addTest(suite);
+  // Définir l'utilitaire de sortie au format "erreurs de compilation"
+  runner.setOutputter(new CppUnit::CompilerOutputter(&runner.result(),
+                      std::cerr));
+  // Lancer l'exécution des tests
+  bool wasSucessful = runner.run();
+  // Retourner le code d'erreur 1 si l'un des tests a échoué
+  return wasSucessful ? 0 : 1;
+}
